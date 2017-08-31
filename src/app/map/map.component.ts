0369657927db3fc01cd435d68b226c18371c83a6
Nicole Freed <nicole.freed@gmail.com>
2017-08-31 08:41:54 -0700

    add preliminary d3 gist code; modify for project

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,6 +15,7 @@ declare var google;
 
 export class MapComponent implements OnInit {
   wildfires: Wildfire[] = [];
+  dataset = this.wildfires;
 
   constructor(private wildfireData: WildfireApiService) { }
 
@@ -29,24 +30,68 @@ export class MapComponent implements OnInit {
       mapTypeId: 'terrain'
     });
 
-    for(var i = 0; i < this.wildfires.length; i++) {
-      var title = this.wildfires[i].title;
-      var description = this.wildfires[i].description;
-      var marker = new google.maps.Marker({
-        position: {lat: this.wildfires[i].lat, lng: this.wildfires[i].lng},
-        map: map,
-        animation: google.maps.Animation.DROP,
-        icon: image,
-        title: title,
-        description: description
-      });
-      marker.addListener('click', function() {
-        var infowindow = new google.maps.InfoWindow({
-          content: this.title + "<br>" + '<a href=' + this.description + '>' + this.description + '</a>'
-        });
-        infowindow.open(map, this);
-      });
-    }
+    var overlay = new google.maps.OverlayView();
+    overlay.onAdd = function() {
+    var layer = d3.select(this.getPanes().overlayLayer).append("div")
+        .attr("class", "wildfires");
+
+        // Draw each marker as a separate SVG element.
+        // We could use a single SVG, but what size would it have?
+        overlay.draw = function() {
+          var projection = this.getProjection(),
+              padding = 10;
+
+          var marker = layer.selectAll("svg")
+              .data(d3.entries(this.dataset))
+              .each(transform) // update existing markers
+              .enter().append("svg")
+              .each(transform)
+              .attr("class", "marker");
+
+          // Add a circle.
+          marker.append("circle")
+              .attr("r", 4.5)
+              .attr("cx", padding)
+              .attr("cy", padding);
+
+          // Add a label.
+          marker.append("text")
+              .attr("x", padding + 7)
+              .attr("y", padding)
+              .attr("dy", ".31em")
+              .text(function(d) { return d.key; });
+
+          function transform(d) {
+            d = new google.maps.LatLng(oregon[1], oregon[0]);
+            d = projection.fromLatLngToDivPixel(d);
+            return d3.select(this)
+                .style("left", (d.x - padding) + "px")
+                .style("top", (d.y - padding) + "px");
+          }
+        };
+      };
+
+      // Bind our overlay to the map…
+      overlay.setMap(map);
+
+    // for(var i = 0; i < this.wildfires.length; i++) {
+    //   var title = this.wildfires[i].title;
+    //   var description = this.wildfires[i].description;
+    //   var marker = new google.maps.Marker({
+    //     position: {lat: this.wildfires[i].lat, lng: this.wildfires[i].lng},
+    //     map: map,
+    //     animation: google.maps.Animation.DROP,
+    //     icon: image,
+    //     title: title,
+    //     description: description
+    //   });
+    //   marker.addListener('click', function() {
+    //     var infowindow = new google.maps.InfoWindow({
+    //       content: this.title + "<br>" + '<a href=' + this.description + '>' + this.description + '</a>'
+    //     });
+    //     infowindow.open(map, this);
+    //   });
+    // }
   }
 
   getWildfireData() {
